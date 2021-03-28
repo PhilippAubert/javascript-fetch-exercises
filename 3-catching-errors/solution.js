aa46c43918520c6e94c79f621db635eb4ea1d2d6
Philipp Aubert <p.b.m.aubert@gmail.com>
2021-03-28 22:23:26 +0200

    rick and morty assignment 1+2

--- 3-catching-errors/solution.js
+++ 3-catching-errors/solution.js
@@ -6,6 +6,7 @@ fetch(url)
   .then((response) => {
     return response.json();
   })
+
   .then((data) => {
     console.log(data);
   })
--- 4-using-the-data/solution.js
+++ 4-using-the-data/solution.js
@@ -4,23 +4,32 @@ const url = `https://randomuser.me/api/?inc=gender,name,picture&results=10`;
 
 fetch(url)
   .then((response) => {
-    return response.json();
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw Error("ERROR!");
+    }
   })
   .then((data) => {
-    data.results.forEach((person) => {
-      const main = document.querySelector("main");
-      const section = document.createElement("section");
-      section.classList.add("person");
+    console.log(data.results);
+    data.results
+      .forEach((person) => {
+        const main = document.querySelector("main");
+        const section = document.createElement("section");
+        section.classList.add("person");
 
-      const nameFull = document.createElement("h2");
-      nameFull.textContent = `${person.name.first} / ${person.name.last}`;
+        const nameFull = document.createElement("h2");
+        nameFull.textContent = `${person.name.first}  ${person.name.last}`;
 
-      const img = document.createElement("img");
-      img.src = person.picture.large;
-      img.alt = `${person.name.first} / ${person.name.last}`;
+        const img = document.createElement("img");
+        img.src = person.picture.large;
+        img.alt = `${person.name.first}  ${person.name.last}`;
 
-      main.append(section);
-      section.append(nameFull);
-      section.append(img);
-    });
+        main.append(section);
+        section.append(nameFull);
+        section.append(img);
+      })
+      .catch((error) => {
+        console.log(error.toString());
+      });
   });
